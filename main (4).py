a7bbf9d8a3ebf04c5a83063d94d476d062f4c4d1
dorwinj2001 <149213146+dorwinj2001@users.noreply.github.com>
2024-03-03 18:00:10 -0600

    I redid the move the function and I have successfully stopped the snake from going out of bounds

--- main (4).py
+++ main (4).py
@@ -45,7 +45,7 @@ def end(game_state: typing.Dict):
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
 
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
+   is_move_safe = {"up": True, "down": True, "left": True, "right": True}
 
     # We've included code to prevent your Battlesnake from moving backwards
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
@@ -53,49 +53,53 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
     if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
-
     elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
         is_move_safe["right"] = False
-
     elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
         is_move_safe["down"] = False
-
     elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
         is_move_safe["up"] = False
 
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    board_width = game_state['board']['width']
-    board_height = game_state['board']['height']
+    # Extract board dimensions
+    width = game_state["board"]["width"]
+    height = game_state["board"]["height"]
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    my_body = game_state['you']['body']
+    head_x = game_state["you"]["head"]["x"]
+    head_y = game_state["you"]["head"]["y"]
 
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    opponents = game_state['board']['snakes']
+    # Available moves (up, down, left, right)
+    possible_moves = ["up", "down", "left", "right"]
+
+    # Removing moves that would lead out of bounds
+    if head_x == 0:
+        possible_moves.remove("left")
+    if head_x == width - 1:
+        possible_moves.remove("right")
+    if head_y == 0:
+        possible_moves.remove("down")
+    if head_y == height - 1:
+        possible_moves.remove("up")
 
     # Are there any safe moves left?
     safe_moves = []
     for move, isSafe in is_move_safe.items():
-        if isSafe:
+        if isSafe and move in possible_moves:  # Check if move is both safe and possible
             safe_moves.append(move)
 
     if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
+        print(f"MOVE {game_state['turn']
+                      }: No safe moves detected! Moving down")
         return {"move": "down"}
 
     # Choose a random move from the safe ones
     next_move = random.choice(safe_moves)
 
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    food = game_state['board']['food']
-
     print(f"MOVE {game_state['turn']}: {next_move}")
+
     return {"move": next_move}
 
-
-
 def evaluation_function(game_state:typing.Dict):
-    
+    pass
 
 
 
